167e90470613567a99eb9430d59dc477f5159b51
Max Severin <makc.severin@gmail.com>
2021-07-04 19:17:59 +0000

    Test and implement SudokuSolver.solve()

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -4,25 +4,32 @@ const SIZE_POW_2 = Math.pow(SIZE, 2);
 
 class SudokuSolver {
   validate(puzzleString) {
-    const regex = new RegExp(`^[0-9|\.]{${SIZE_POW_2}}$`);
+    if (puzzleString === '') {
+      throw new Error('Required field missing');
+    }
 
-    return regex.test(puzzleString);
+    if (puzzleString.length !== SIZE_POW_2) {
+      throw new Error('Expected puzzle to be 81 characters long');
+    }
+
+    if (!/^[0-9.]+$/.test(puzzleString)) {
+      throw new Error('Invalid characters in puzzle');
+    }
+
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
     const from = row * SIZE - SIZE;
     const to = row * SIZE;
-    const rowString = puzzleString.slice(from, from + column - 1)
-                      .concat(puzzleString.slice(from + column, to));
-                      
+    const rowString = puzzleString.slice(from, to);
+
     return !rowString.includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
     let colString = puzzleString.slice(column - 1, column)
                       .concat(puzzleString.slice(column).split('').filter((x, i) => (i + 1) % 9 === 0).join(''));
-
-    colString = colString.slice(0, row - 1).concat(colString.slice(row));
 
     return !colString.includes(value);
   }
@@ -52,19 +59,55 @@ class SudokuSolver {
       for (let j = fromColumn + 1; j <= toColumn; j++) {
         thisIndex = i * SIZE - SIZE + j - 1;
 
-        if (thisIndex !== currentIndex) {
-          regionString += puzzleString.slice(thisIndex, thisIndex + 1);
-        }
+        regionString += puzzleString.slice(thisIndex, thisIndex + 1);
       }
     }
 
     return !regionString.includes(value);
   }
 
+  getEmptyCell(puzzleString) {
+    let currentIndex;
+
+    for (let i = 1; i <= SIZE; i++) {
+      for (let j = 1; j <= SIZE; j++) {
+        currentIndex = i * SIZE - SIZE + j - 1;
+
+        if (puzzleString[currentIndex] === '.') {
+          return [i, j];
+        }
+      }
+    }
+
+    return [-1, -1]
+  }
+
   solve(puzzleString) {
-    
+    this.validate(puzzleString);
+
+    let [row, column] = this.getEmptyCell(puzzleString);
+    let currentIndex = row * SIZE - SIZE + column - 1;
+
+    if (row === -1) {
+      return puzzleString;
+    }
+
+    for (let i = 1; i <= 9; i++) {
+      if (this.checkRowPlacement(puzzleString, row, column, i) 
+          && this.checkColPlacement(puzzleString, row, column, i)
+          && this.checkRegionPlacement(puzzleString, row, column, i)) {
+            puzzleString = puzzleString.slice(0, currentIndex).concat(i).concat(puzzleString.slice(currentIndex + 1));
+
+            puzzleString = this.solve(puzzleString);
+      }
+    }
+
+    if (this.getEmptyCell(puzzleString)[0] !== -1) {
+      puzzleString = puzzleString.slice(0, currentIndex).concat('.').concat(puzzleString.slice(currentIndex + 1));
+    }
+
+    return puzzleString;
   }
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -13,29 +13,39 @@ suite('UnitTests', () => {
 
   test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', function () {
     let testString = 'a.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
-    assert.isFalse(solver.validate(testString));
+    assert.throws(() => {
+      solver.validate(testString);
+    }, 'Invalid characters in puzzle');
 
     testString = '*.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
-    assert.isFalse(solver.validate(testString));
+    assert.throws(() => {
+      solver.validate(testString);
+    }, 'Invalid characters in puzzle');
 
     testString = '-.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
-    assert.isFalse(solver.validate(testString));
+    assert.throws(() => {
+      solver.validate(testString);
+    }, 'Invalid characters in puzzle');
   });
 
   test('Logic handles a puzzle string that is not 81 characters in length', function () {
     let testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37';
-    assert.isFalse(solver.validate(testString));
+    assert.throws(() => {
+      solver.validate(testString);
+    }, 'Expected puzzle to be 81 characters long');
 
     testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37..';
-    assert.isFalse(solver.validate(testString));
+    assert.throws(() => {
+      solver.validate(testString);
+    }, 'Expected puzzle to be 81 characters long');
   });
 
   test('Logic handles a valid row placement', function () {
     let testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
-    assert.isTrue(solver.checkRowPlacement(testString, 1, 1, 1));
     assert.isTrue(solver.checkRowPlacement(testString, 1, 2, 3));
-    assert.isTrue(solver.checkRowPlacement(testString, 1, 3, 5));
-    assert.isTrue(solver.checkRowPlacement(testString, 4, 2, 4));
+    assert.isTrue(solver.checkRowPlacement(testString, 2, 5, 8));
+    assert.isTrue(solver.checkRowPlacement(testString, 4, 3, 8));
+    assert.isTrue(solver.checkRowPlacement(testString, 7, 4, 9));
   });
 
   test('Logic handles an invalid row placement', function () {
@@ -48,10 +58,10 @@ suite('UnitTests', () => {
 
   test('Logic handles a valid column placement', function () {
     let testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
-    assert.isTrue(solver.checkColPlacement(testString, 1, 1, 1));
-    assert.isTrue(solver.checkColPlacement(testString, 3, 2, 2));
-    assert.isTrue(solver.checkColPlacement(testString, 4, 2, 9));
-    assert.isTrue(solver.checkColPlacement(testString, 8, 9, 9));
+    assert.isTrue(solver.checkColPlacement(testString, 2, 2, 4));
+    assert.isTrue(solver.checkColPlacement(testString, 3, 4, 2));
+    assert.isTrue(solver.checkColPlacement(testString, 4, 7, 1));
+    assert.isTrue(solver.checkColPlacement(testString, 5, 9, 2));
   });
 
   test('Logic handles an invalid column placement', function () {
@@ -64,9 +74,9 @@ suite('UnitTests', () => {
 
   test('Logic handles a valid region (3x3 grid) placement', function () {
     let testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
-    assert.isTrue(solver.checkRegionPlacement(testString, 1, 2, 3));
-    assert.isTrue(solver.checkRegionPlacement(testString, 2, 7, 2));
-    assert.isTrue(solver.checkRegionPlacement(testString, 4, 3, 1));
+    assert.isTrue(solver.checkRegionPlacement(testString, 1, 7, 6));
+    assert.isTrue(solver.checkRegionPlacement(testString, 2, 8, 1));
+    assert.isTrue(solver.checkRegionPlacement(testString, 6, 4, 7));
     assert.isTrue(solver.checkRegionPlacement(testString, 8, 8, 2));
   });
 
@@ -78,16 +88,50 @@ suite('UnitTests', () => {
     assert.isFalse(solver.checkRegionPlacement(testString, 8, 8, 1));
   });
 
-  // test('Valid puzzle strings pass the solver', function () {
-  //   //
-  // });
+  test('Valid puzzle strings pass the solver', function () {
+    let testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+    let response = solver.solve(testString);
+    assert.isString(response);
+    assert.notEqual(response, testString);
+  });
 
-  // test('Invalid puzzle strings fail the solver', function () {
-  //   //
-  // });
+  test('Invalid puzzle strings fail the solver', function () {
+    assert.throws(() => {
+      let testString = '';
+      let response = solver.solve(testString);
+    }, 'Required field missing');
 
-  // test('Solver returns the expected solution for an incomplete puzzle', function () {
-  //   //
-  // });
+    assert.throws(() => {
+      let testString = 'a.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+      let response = solver.solve(testString);
+    }, 'Invalid characters in puzzle');
+    
+    assert.throws(() => {
+      let testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37';
+      let response = solver.solve(testString);
+    }, 'Expected puzzle to be 81 characters long');
+  });
+
+  test('Solver returns the expected solution for an incomplete puzzle', function () {    
+    let testString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+    let solvedString = '135762984946381257728459613694517832812936745357824196473298561581673429269145378';
+    assert.equal(solver.solve(testString), solvedString);
+
+    testString = '5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3';
+    solvedString = '568913724342687519197254386685479231219538467734162895926345178473891652851726943';
+    assert.equal(solver.solve(testString), solvedString);
+    
+    testString = '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1';
+    solvedString = '218396745753284196496157832531672984649831257827549613962415378185763429374928561';
+    assert.equal(solver.solve(testString), solvedString);
+    
+    testString = '.7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6';
+    solvedString = '473891265851726394926345817568913472342687951197254638734162589685479123219538746';
+    assert.equal(solver.solve(testString), solvedString);
+    
+    testString = '82..4..6...16..89...98315.749.157.............53..4...96.415..81..7632..3...28.51';
+    solvedString = '827549163531672894649831527496157382218396475753284916962415738185763249374928651';
+    assert.equal(solver.solve(testString), solvedString);
+  });
 
 });
